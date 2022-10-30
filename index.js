96063f22a07fc4f883f28486ace50569ddbe095d
muzaffar640 <muzaffar640@gmail.com>
2022-10-30 11:26:11 +0530

    update alert msg, university name and add guide in bengali

--- index.js
+++ index.js
@@ -4,5 +4,13 @@ function formSubmit(event) {
   const name = document.sgpaCal.Name.value;
   const avgCgpa = (parseFloat(firstSGPA) + parseFloat(secondSGPA)) / 2;
   const semPercentage = avgCgpa * 10 - 5;
-  alert(`Dear ${name}, your percentage is ${semPercentage.toFixed(2)}`);
+  if (semPercentage > 100) {
+    alert(
+      `WARNING: প্রিয় ${name}, আপনার percentage হলো ${semPercentage.toFixed(
+        2
+      )}। আপনার শতাংশ 100-এর বেশি হওয়া উচিত নয়। অনুগ্রহ করে আপনি যে SGPA দিয়েছেন তা যাচাই করুন।`
+    );
+  } else {
+    alert(`Dear ${name}, your percentage is ${semPercentage.toFixed(2)}`);
+  }
 }
